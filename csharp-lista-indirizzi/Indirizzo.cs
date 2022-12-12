f906e4b0ba0dde8ec6543c2ea7e1b2f71fcabf2c
L-V-Dragon <viotti37@gmail.com>
2022-12-12 15:29:55 +0100

    Add project files.

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzo
+    {
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string province;
+        private string zip;
+
+        public Indirizzo(string name, string surname, string street, string city, string province, string zip)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.zip = zip;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public string GetSurname()
+        {
+            return this.surname;
+        }
+
+        public string GetStreet()
+        {
+            return this.street;
+        }
+
+        public string GetCity()
+        {
+            return this.city;
+        }
+
+        public string GetProvince()
+        {
+            return this.province;
+        }
+
+        public string GetZip()
+        {
+            return this.zip;
+        }
+
+
+    }
+}
